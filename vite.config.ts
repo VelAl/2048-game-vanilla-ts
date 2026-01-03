34b15d5a55e35d30ae8fcbb5059fc9480e6b1f42
Alex <welmozhko88@gmail.com>
2026-01-03 13:03:10 +0200

    set up the project

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    /* 
+      Automatically opens the app in a new browser tab when the dev server is started.
+    */
+    open: true,
+  },
+});
